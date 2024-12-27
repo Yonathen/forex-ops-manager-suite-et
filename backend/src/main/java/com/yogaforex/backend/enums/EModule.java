a700dd02b7830495ac588261e9dd9497edac85bc
Yonathen <yonstudy18@gmail.com>
2024-12-27 22:45:21 +0100

    Revert "Feature 9 integrate swagger documentation"

--- backend/src/main/java/com/yogaforex/backend/enums/EModule.java
+++
@@ -1,8 +0,0 @@
-package com.yogaforex.backend.enums;
-
-public enum EModule {
-    USER_MANAGEMENT,
-    TRANSACTION_MANAGEMENT,
-    REPORT_MANAGEMENT,
-    EXCHANGE_RATES_MANAGEMENT,
-}
--- backend/src/main/java/com/yogaforex/backend/enums/EStatus.java
+++
@@ -1,7 +0,0 @@
-package com.yogaforex.backend.enums;
-
-public enum EStatus {
-    ACTIVE,
-    INACTIVE,
-    DELETED
-}
