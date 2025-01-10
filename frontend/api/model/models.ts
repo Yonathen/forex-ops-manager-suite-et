13611e6448f4657067e4eebcf1ca22573daa3513
Yonathen <yonstudy18@gmail.com>
2025-01-10 22:26:09 +0100

    Generate angular api services for communicating with backend

---
+++ frontend/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './address';
+export * from './branchDto';
+export * from './paginatedResponseDTOUserPublicDto';
+export * from './permissionDto';
+export * from './roleDto';
+export * from './userDto';
+export * from './userPublicDto';
